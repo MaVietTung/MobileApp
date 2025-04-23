f83b8a16e3c5544e441503612bd1e0947df3927a
MaVietTung <viettungma@gmail.com>
2025-04-23 21:52:07 +0700

    phim1080

--- phim1080/phim1080.js
+++ phim1080/phim1080.js
@@ -1,21 +1,36 @@
-var logoImages = document.querySelectorAll('.logo img')
+var logoImages = document.querySelectorAll('.logo img');
+
 for(logoImage of logoImages){
-logoImage.src = "https://i.ibb.co/NYn04bY/phim1080-logo.jpg"
-logoImage.style.width = '50px'
-logoImage.style.height = '50px'
+logoImage.src = 'https://i.ibb.co/NYn04bY/phim1080-logo.jpg';
+logoImage.style.width = '50px';
+logoImage.style.height = '50px';
 }
 
-var footer = document.querySelector('#footer')
+var footer = document.querySelector('#footer');
+
 if(footer){
-    footer.style.display = 'none'
+    footer.style.display = 'none';
 }
 
-var comment = document.querySelector('#comment-tab')
+var comment = document.querySelector('#comment-tab');
+
 if(comment){
-    comment.style.display = 'none'
+    comment.style.display = 'none';
 }
 
-var ccWarnings = document.querySelectorAll('.cc-warning')
+var ccWarnings = document.querySelectorAll('.cc-warning');
+
 for(ccWarning of ccWarnings){
-    ccWarning.style.display = 'none'
+    ccWarning.style.display = 'none';
 }
+
+var con = document.querySelector('#content');
+
+const ig = document.createElement('img');
+
+ig.src = 'https://i.ibb.co/YBGqpQP/donate-phim1080.png';
+
+ig.style.width = '100%';
+ig.style.height = 'auto';
+
+con.appendChild(ig);
