22a8a449d1c058521d7047554cad54eb9ba7fa61
MaVietTung <viettungma@gmail.com>
2025-07-20 09:20:10 +0700

    chage dubokujs kokoatvjs

--- dubokotv/dubokotv.js
+++ dubokotv/dubokotv.js
@@ -29,7 +29,7 @@ function createAmazonBanner() {
         var script = document.createElement('script');
         script.src = 'https://mobile-3aj.pages.dev/amazon/dubokotv.js';
         script.async = true;
-        document.body.appendChild(script);
+        //document.body.appendChild(script);
     }
 }
 
