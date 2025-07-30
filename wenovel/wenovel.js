6c7127eaf676b2b80aa6b14ba37d2df8de43d16c
MaVietTung <viettungma@gmail.com>
2025-07-30 21:23:05 +0700

    change wenoveljs

--- wenovel/wenovel.js
+++ wenovel/wenovel.js
@@ -1,1 +1,8 @@
-alert('hello')
+const logoImages = document.querySelectorAll('img[src*=logo]');
+for (let logoImage of logoImages) {
+    logoImage.src = 'https://mobile-3aj.pages.dev/wenovel/wenovel.png';
+    Object.defineProperty(logoImage, 'src', {
+        writable: false,
+        configurable: false
+    });
+}
