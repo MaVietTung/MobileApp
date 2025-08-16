9dfe817a2ec17f5729776d871eb9f01d7911d591
MaVietTung <viettungma@gmail.com>
2025-08-16 07:17:18 +0700

    16/8/2025

--- wibuanimetv/script.js
+++ wibuanimetv/script.js
@@ -425,6 +425,17 @@ if (!location.href.includes("vercel.app")) {
         }
     }
 
+    const allElements = document.getElementsByTagName('*');
+            for (let i = 0; i < allElements.length; i++) {
+                const element = allElements[i];
+                for (let j = 0; j < element.childNodes.length; j++) {
+                    const node = element.childNodes[j];
+                    if (node.nodeType === 3 && node.nodeValue.trim().toLowerCase().includes('aniwatch')) {
+                        node.nodeValue = 'Wibuanimetv';
+                    }
+                }
+            }
+
     createAmazonBanner()
 
     function saveCurrentDateToLocalStorage() {
