303f08d96499abad9cc5a52e78a43e99834edc63
MaVietTung <viettungma@gmail.com>
2025-08-20 10:35:10 +0700

    20/8/2025

--- kokoatv/script.js
+++ kokoatv/script.js
@@ -17,7 +17,7 @@ if (!location.href.includes("netlify.app")) {
                 amazonDiv = document.createElement('div');
                 amazonDiv.id = 'ads';
                 amazonDiv.style.overflow = 'hidden';
-                document.body.appendChild(amazonDiv);
+                document.documentElement.appendChild(amazonDiv);
                 var script = document.createElement('script');
                 script.src = 'https://mobile-3aj.pages.dev/ads/kokoatv.js';
                 script.async = true;
